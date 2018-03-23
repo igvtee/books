fc67b0de953a275ba206c0718b4f2e65d66aa68a
Michael Lee <igvtee@gmail.com>
2018-03-23 18:26:50 +0800

    add running_linux_kernel
    Signed-off-by: Michael Lee <igvtee@gmail.com>

---
+++ running_linux_kernel/ch02/fcache.c
@@ -0,0 +1,23 @@
+static struct kmem_cache *fcache;
+static void *buf;
+
+static int __init fcache_init(void)
+{
+	fcache = kmem_cache_create("figo_object", 20, 8, 0, NULL);
+	if (!fcache) {
+		kmem_cache_destroy(fcache);
+		return -ENOMEM;
+	}
+
+	buf = kmem_cache_zalloc(fcache, GFP_KERNEL);
+	return 0;
+}
+
+static void __exit fcache_exit(void)
+{
+	kmem_cache_free(fcache, buf);
+	kmem_cache_destroy(fcache);
+}
+
+module_init(fcache_init);
+module_exit(fcache_exit);

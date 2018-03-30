330ae9736e459d399c618ea01fab5a2c9bc43cae
Michael Lee <igvtee@gmail.com>
2018-03-30 10:12:54 +0800

    update running_linux_kernel ch02 module makefile
    Signed-off-by: Michael Lee <igvtee@gmail.com>

--- running_linux_kernel/ch02/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export ARCH=arm
-export CROSS_COMPILE=arm-linux-gnueabi-
-KERNEL_PATH=~/programs/running_linux_kernel/runninglinuxkernel_4.0/
-echo ${KERNEL_PATH}
-make -C ${KERNEL_PATH} M=`pwd` clean
-make -C ${KERNEL_PATH} M=`pwd` modules

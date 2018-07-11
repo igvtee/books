17099acff0fa4894b39dbddc12866b280c799e23
Michael Lee <igvtee@gmail.com>
2018-07-11 11:58:02 +0800

    qemu_linux update use with gdb
    Signed-off-by: Michael Lee <igvtee@gmail.com>

--- qemu_linux/arm32.sh
+++ qemu_linux/arm32.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 qemu-system-arm -M vexpress-a9 -smp 4 -m 512M -kernel zImage.arm32 -initrd initramfs.cpio.gz -append "rdinit=/linuxrc console=ttyAMA0 loglevel=8 earlyprintk" -dtb linux-4.0/arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic
+
+# gdb
+# arm-linux-gnueabihf-gdb ./vmlinux
+# target remote 127.0.0.1
+# b start_kernel
+#qemu-system-arm -S -s -M vexpress-a9 -smp 4 -m 512M -kernel zImage.arm32 -initrd initramfs.cpio.gz -append "rdinit=/linuxrc console=ttyAMA0 loglevel=8 earlyprintk" -dtb linux-4.0/arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic

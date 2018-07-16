3b2877c179e73643e6565bc6e4224a8c481ef482
Michael Lee <igvtee@gmail.com>
2018-07-16 23:25:04 +0800

    add gdb remote port number
    Signed-off-by: Michael Lee <igvtee@gmail.com>

--- qemu_linux/arm32.sh
+++ qemu_linux/arm32.sh
@@ -3,6 +3,6 @@ qemu-system-arm -M vexpress-a9 -smp 4 -m 512M -kernel zImage.arm32 -initrd initr
 
 # gdb
 # arm-linux-gnueabihf-gdb ./vmlinux
-# target remote 127.0.0.1
+# target remote 127.0.0.1:1234
 # b start_kernel
 #qemu-system-arm -S -s -M vexpress-a9 -smp 4 -m 512M -kernel zImage.arm32 -initrd initramfs.cpio.gz -append "rdinit=/linuxrc console=ttyAMA0 loglevel=8 earlyprintk" -dtb linux-4.0/arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic

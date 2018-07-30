90be637e90fbfb117af08b6633f40c7e18a82459
Michael Lee <igvtee@gmail.com>
2018-07-30 21:43:45 +0800

    [qemu_linux] use gdb debug kernel module
    Signed-off-by: Michael Lee <igvtee@gmail.com>

--- qemu_linux/arm32.sh
+++ qemu_linux/arm32.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 qemu-system-arm -M vexpress-a9 -smp 4 -m 512M -kernel zImage.arm32 -initrd initramfs.cpio.gz -append "rdinit=/linuxrc console=ttyAMA0 loglevel=8 earlyprintk" -dtb linux-4.0/arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic
 
-# gdb
-# arm-linux-gnueabihf-gdb ./vmlinux
+# kernel gdb
+# arm-linux-gnueabi-gdb ./vmlinux
 # target remote 127.0.0.1:1234
 # b start_kernel
+#
+# kernel module gdb
+# b sys_init_module
+# modprobe sequence
+# cat /sys/module/sequence/sections/.text (0x7f000000)
+# add-symbol-file ../../linux_device_drivers/misc-modules/sequence.ko 0x7f000000
 #qemu-system-arm -S -s -M vexpress-a9 -smp 4 -m 512M -kernel zImage.arm32 -initrd initramfs.cpio.gz -append "rdinit=/linuxrc console=ttyAMA0 loglevel=8 earlyprintk" -dtb linux-4.0/arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic

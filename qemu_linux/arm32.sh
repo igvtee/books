01f4901d02f0968df0115f73a0be10c9e9dd6617
Michael Lee <igvtee@gmail.com>
2018-08-04 23:25:23 +0800

    [qemu_linux] update kernel module debug with bss data info
    Signed-off-by: Michael Lee <igvtee@gmail.com>

--- qemu_linux/arm32.sh
+++ qemu_linux/arm32.sh
@@ -10,5 +10,7 @@ qemu-system-arm -M vexpress-a9 -smp 4 -m 512M -kernel zImage.arm32 -initrd initr
 # b sys_init_module
 # modprobe sequence
 # cat /sys/module/sequence/sections/.text (0x7f000000)
-# add-symbol-file ../../linux_device_drivers/misc-modules/sequence.ko 0x7f000000
+# cat /sys/module/sequence/sections/.bss (0x7f0023dc)
+# cat /sys/module/sequence/sections/.data (0x7f001f58)
+# add-symbol-file ../../linux_device_drivers/misc-modules/sequence.ko 0x7f000000 -s .bss 0x7f0023dc -s .data 0x7f001f58
 #qemu-system-arm -S -s -M vexpress-a9 -smp 4 -m 512M -kernel zImage.arm32 -initrd initramfs.cpio.gz -append "rdinit=/linuxrc console=ttyAMA0 loglevel=8 earlyprintk" -dtb linux-4.0/arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic

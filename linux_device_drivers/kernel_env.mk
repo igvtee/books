6d9a9f1e70cc2dbefbe09c85b13530926c9797a3
Michael Lee <igvtee@gmail.com>
2018-09-16 12:10:58 +0800

    [linux_device_drivers] fix user program install path
    Signed-off-by: Michael Lee <igvtee@gmail.com>

--- linux_device_drivers/kernel_env.mk
+++ linux_device_drivers/kernel_env.mk
@@ -2,8 +2,9 @@ ARCH ?= arm
 CROSS_COMPILE ?= arm-linux-gnueabi-
 # Assume the source tree is where the running kernel was built
 # You should set KERNELDIR in the environment if it's elsewhere
-KERNELDIR ?= ../../qemu_linux/linux-4.0
-INSTALLDIR ?= ../../qemu_linux/rootfs
+KERNELDIR ?= ../../qemu_linux/build/${ARCH}/linux
+INSTALLDIR ?= ../../../rootfs
+PROGINSTALLDIR ?= ../../qemu_linux/rootfs
 
 export ARCH CROSS_COMPILE
 
@@ -19,6 +20,6 @@ clean:
 
 install:
 	INSTALL_MOD_PATH=$(INSTALLDIR) $(MAKE) -C $(KERNELDIR) M=$(CURDIR) modules_install
-	@[ -z "$(CROSS_PROGS)" ] || cp $(CROSS_PROGS) -f $(INSTALLDIR)/usr/bin/
-	@[ -z "$(SCRIPT_FILES)" ] || cp $(SCRIPT_FILES) -f $(INSTALLDIR)/usr/bin/
-	@[ -z "$(SCRIPT_FILES)" ] || ( cd $(INSTALLDIR)/usr/bin/; chmod +x $(SCRIPT_FILES) )
+	@[ -z "$(CROSS_PROGS)" ] || cp $(CROSS_PROGS) -f $(PROGINSTALLDIR)/usr/bin/
+	@[ -z "$(SCRIPT_FILES)" ] || cp $(SCRIPT_FILES) -f $(PROGINSTALLDIR)/usr/bin/
+	@[ -z "$(SCRIPT_FILES)" ] || ( cd $(PROGINSTALLDIR)/usr/bin/; chmod +x $(SCRIPT_FILES) )

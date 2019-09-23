e6ea0af72d5b16971577648642963c5f6fec25f7
Michael Lee <igvtee@gmail.com>
2019-09-23 18:25:12 +0800

    [openwrt] enable debug info
    Signed-off-by: Michael Lee <igvtee@gmail.com>

--- openwrt/example/safe_list.c
+++ openwrt/example/safe_list.c
@@ -14,20 +14,43 @@ void number_add(struct safe_list *head, int num)
 	safe_list_add(&new->list, head);
 }
 
-int print_cb(void *ctx __attribute__((unused)), struct safe_list *list)
+void number_print(struct safe_list *head)
 {
 	struct number *num;
 
+	list_for_each_entry(num, &head->list, list.list)
+		printf("%d ", num->num);
+	putchar('\n');
+}
+
+int del_num_cb(void *ctx __attribute__((unused)), struct safe_list *list)
+{
+	struct number *num;
+	int del_num = (intptr_t) ctx;
+
 	num = list_entry(list, __typeof__(*num), list);
-	printf("%d ", num->num);
+	if (num->num == del_num) {
+		safe_list_del(list);
+		free(num);
+	}
 
 	return 0;
 }
 
-void number_print(struct safe_list *head)
+void number_del_num(struct safe_list *head, intptr_t del_num)
 {
-	safe_list_for_each(head, print_cb, NULL);
-	putchar('\n');
+	safe_list_for_each(head, del_num_cb, (void *) del_num);
+}
+
+void number_clean(struct safe_list *head)
+{
+	struct number *num;
+
+	while (!list_empty(&head->list)) {
+		num = list_first_entry(&head->list, struct number, list.list);
+		safe_list_del(&num->list);
+		free(num);
+	}
 }
 
 int main(void)
@@ -38,6 +61,11 @@ int main(void)
 	for (i = 0; i < 5; i++)
 		number_add(&list, i);
 	number_print(&list);
+
+	number_del_num(&list, 3);
+	number_print(&list);
+
+	number_clean(&list);
 
 	return 0;
 }
